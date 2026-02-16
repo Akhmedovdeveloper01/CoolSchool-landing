e00cf90abdaee65fd3e39bce164cd3542d0c08f1
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2026-02-16 21:54:09 +0500

    ....

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,7 +10,7 @@ import { Header } from "./components/Header";
 export default function App() {
   return (
     <div className="min-h-screen bg-white">
-      <Header />
+      {/* <Header /> */}
       <Hero />
       <WhyUs />
       <HowItWorks />
